783ad0e30873e1a299156111f745fa21bff7eca6
disys-vinayak <95425035+disys-vinayak@users.noreply.github.com>
2021-12-12 23:38:23 +0530

    Add files via upload

---
+++ text to json.py
@@ -0,0 +1,26 @@
+text_to_json = [
+    {
+        'id': 1000,
+        'name': 'John Smith',
+        'DOB': '01/01/2000'
+        'gender': 'F'
+        'age': 20,
+        'Zip Code': '08122-2324',
+        'Amount'; 1500.20},
+    {
+        'id': 2000,
+        'name': 'Jim McDonald',
+        'DOB': '02/02/2020'
+        'gender': 'M'
+        'age': 25,
+        'Zip Code': '08136-2324',
+        'Amount'; 1500.20},
+    {
+        'id': 20,
+        'name': 'Jim McDonald',
+        'DOB': '02/02/1999'
+        'gender': 'F'
+        'age': 20,
+        'Zip Code': '08124-6565',
+        'Amount'; 1500.20}
+    ]
