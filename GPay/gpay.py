27cb793c53edc5408fc56fd8f1f7677efb779041
disys-vinayak <95425035+disys-vinayak@users.noreply.github.com>
2021-12-14 06:32:57 +0530

    Update gpay.py

--- GPay/gpay.py
+++ GPay/gpay.py
@@ -4,6 +4,7 @@ class gpay:
         self.__mail = ''
         self.__num = 0
         self.__pin = 0
+        self.__pan = ''
     
     def enter_mail(self, mail: str):
         if not isinstance(mail, str):
@@ -21,5 +22,21 @@ class gpay:
                 raise ValueError('Num should be 10 digits long')
         else:
             raise TypeError('Num should be int')
+    
+    def enter_num(self, name: str):
+        if isinstance(name, str):
+            self.__name = name
+        else:
+            raise TypeError('Name should be string')
+        
+    def enter_pan(self, pan: str):
+        if isinstance(pan, str):
+            if len(pan) == 10:
+                self.__pan = pan
+            else:
+                raise ValueError('PAN should be 10 digits long')
+        else:
+            raise TypeError('PAN should be string')
         
     
+       
