3fb40e1af17911a64b8e00221a38295ec8cb3ad6
disys-vinayak <95425035+disys-vinayak@users.noreply.github.com>
2021-12-15 07:15:09 +0530

    Add files via upload

---
+++ Tracking/tracking.py
@@ -0,0 +1,5 @@
+phone_apps = [
+    {'App Name': 'Whatsapp','Type': 'Comm', 'Hours per day': .5, 'Size': '2gb', 'Cost': 0.0},
+    {'App Name': 'Photos', 'Type': 'Photo', 'Hours per day': .1, 'Size': '1.5gb', 'Cost': 0.0, 'Number of Photos': 365},
+    {''}
+    ]
