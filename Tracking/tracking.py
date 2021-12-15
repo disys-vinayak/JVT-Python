5b94c609250d51645a6d8a3dc7bad0267e243b9d
disys-vinayak <95425035+disys-vinayak@users.noreply.github.com>
2021-12-15 21:04:45 +0530

    Update tracking.py

--- Tracking/tracking.py
+++ Tracking/tracking.py
@@ -1,5 +1,63 @@
-phone_apps = [
-    {'App Name': 'Whatsapp','Type': 'Comm', 'Hours per day': .5, 'Size': '2gb', 'Cost': 0.0},
-    {'App Name': 'Photos', 'Type': 'Photo', 'Hours per day': .1, 'Size': '1.5gb', 'Cost': 0.0, 'Number of Photos': 365},
-    {''}
-    ]
+class App:
+    __permission_lst = ['camera', 'microphone', 'contacts', 'gps', 'sms', 'log']
+    def __init__(self):
+        self.__name = ''
+        self.__hours = 0.0 
+        self.__size = 0.0
+        self.__permissions = []
+        self.__extra = {}
+        
+    def validate_name(self, name):
+        flag = 0
+        if type(name) is not str:
+            flag += 1
+            raise TypeError('App Name must be in string')
+        
+        if flag == 0:
+            return name
+    
+    def validate_hours(self, hours):
+        flag = 0
+        if type(hours) is int:
+            hours = float(hours)
+        if type(hours) is not float:
+            flag += 1
+            raise TypeError('Hours must be in float')
+        if hours > 24.0:
+            flag += 1
+            raise ValueError('Hours Per Day cannot be over 24')
+        if hours < 0.0:
+            flag += 1
+            raise ValueError('Hours Per Day cannot be less than 0')
+        
+        if flag == 0:
+            return hours
+        
+    def validate_size(self, size):
+        flag = 0
+        if type(size) is int:
+            size = float(size)
+        if type(size) is not float:
+            flag += 1
+            raise TypeError('Size must be in float')
+        if size < 0.0:
+            flag += 1
+            raise ValueError('Size cannot be less than 0')
+        
+        if flag == 0:
+            return size
+        
+    def validate_permission(self, permission):
+        flag = 0
+        if type(permission) is not str:
+            flag += 1
+            raise TypeError('Permission must be string')
+        if len(permission) < 1:
+            flag += 1
+            raise ValueError('Permission cannot be less than 1 character long')
+        if permission not in App.__permission_lst:
+            flag += 1
+            raise ValueError('Permission does not exist')
+        
+        if flag == 0:
+            return permission
