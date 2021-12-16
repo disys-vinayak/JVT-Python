a1f42ade62a1032576c91f5bc1789b52e0fd5cdc
disys-vinayak <95425035+disys-vinayak@users.noreply.github.com>
2021-12-16 14:06:35 +0530

    Update phone_pay.py

--- GPay/phone_pay.py
+++ GPay/phone_pay.py
@@ -5,9 +5,10 @@ class PhonePay(gpay.GPay):
     def __init__(self):
         super().__init__()
 
-
+        
 obj = PhonePay()
-obj.create_account('Name', 'mail@.com', '1234567890', '123456789012', '1234567890')
+obj.create_account('Name', 'mail@.com', '1234567890', '123456789012', '1234567890', '2001')
+obj.login('1234567890', '2001')
 obj.add_balance(100000.1)
 obj.add_friend('ABC', '123456789012')
 obj.transfer_money('ABC', '123456789012', 5000.2)
